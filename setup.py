580d01620102d14375f12059c603ff5626de3f2b
Travis Clarke <travis.m.clarke@gmail.com>
2018-12-02 00:58:33 -0800

    v0.0.1 - initial release

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+from pprintast import VERSION
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pprintast",
+    version=VERSION,
+    author="Travis Clarke, Alex Leone",
+    author_email="travis.m.clarke@gmail.com, acleone@gmail.com",
+    description="A AST pretty printer for python.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/clarketm/pprintast",
+    packages=setuptools.find_packages(),
+    py_modules=["pprintast"],
+    entry_points={"console_scripts": ["pprintast=pprintast:cli"]},
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Environment :: Console",
+    ],
+)
