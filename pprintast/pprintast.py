caac73e72a5b70fd7f0d71843a2bd1801a8b91ba
Travis Clarke <travis.m.clarke@gmail.com>
2018-12-23 18:08:16 -0800

    v1.1.2 - include LICENSE and README in dist

--- pprintast/pprintast.py
+++ pprintast/pprintast.py
@@ -6,7 +6,7 @@ the ast.dump function and modified slightly to pretty-print.
 from ast import *
 from enum import Enum
 
-__VERSION__ = "1.1.1"
+__VERSION__ = "1.1.2"
 
 
 class StringEnum(str, Enum):
